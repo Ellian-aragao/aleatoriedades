a9fef6819d2c7edc865a4c281be8ce98acbda1bd
ellian-aragao <ellian.aragao@gmail.com>
2020-02-14 00:12:14 -0300

    atualização do cógido para programas utilizados na instalação

--- shellScript/primeiraInstalacao.sh
+++ shellScript/primeiraInstalacao.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 apt-get install vim -y		    # editor de texto via terminal
-apt-get install qbittorrent     # donwload de torrent
-#apt-get install wireshark      # analisador de pacotes da rede
-#apt-get install calibre        # editor e leitor de ebooks
+apt-get install git -y		    # gerenciador de versionamento
+apt-get install qbittorrent -y  # donwload de torrent
+apt-get install wireshark -y    # analisador de pacotes da rede
+apt-get install calibre -y      # editor e leitor de ebooks
 
 
 snap install code --classic	    # editor de codigos vscode
-snap install gimp		        # composição de fotos
-snap install vlc		        # reprodutor de video e musica
 snap install okular             # leitor de PDF
-# snap install audacity		    # edição de audio
-# snap install darktable	    # editor de fotos
-# snap install xmind		    # mapa mental
-# snap install inkscape		    # editor vetorial
-# snap install kdenlive		    # editor de videos
-# snap install obs-studio       # gravador de tela
+snap install vlc 		        # reprodutor de video e musica
+snap install audacity 		    # edição de audio
+snap install gimp 		        # composição de fotos
+snap install darktable 	    	# editor de fotos
+snap install inkscape 		    # editor vetorial
+#snap install xmind 		    # mapa mental
+#snap install kdenlive 		    # editor de videos
+#snap install obs-studio    	# gravador de tela
 
 
 echo "baixando e atualizando os pacotes"
