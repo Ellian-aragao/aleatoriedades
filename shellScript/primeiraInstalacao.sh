3e674a948bfdb996e529d7767eb4a62c2c57f4e6
ellian-aragao <ellian.aragao@gmail.com>
2020-02-15 00:21:34 -0300

    identando o código e add tweak tool

--- shellScript/primeiraInstalacao.sh
+++ shellScript/primeiraInstalacao.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-apt-get install vim -y		    # editor de texto via terminal
-apt-get install git -y		    # gerenciador de versionamento
-apt-get install qbittorrent -y  # donwload de torrent
-apt-get install wireshark -y    # analisador de pacotes da rede
-apt-get install calibre -y      # editor e leitor de ebooks
-apt-get install default-jdk -y	# pacote java atual
+apt-get install gnome-tweak-tool -y	# configurar aparencia do gnome
+apt-get install vim -y		    	# editor de texto via terminal
+apt-get install git -y		    	# gerenciador de versionamento
+apt-get install qbittorrent -y  	# donwload de torrent
+apt-get install wireshark -y    	# analisador de pacotes da rede
+apt-get install calibre -y      	# editor e leitor de ebooks
+apt-get install default-jdk -y		# pacote java atual
 
-snap install code --classic	    # editor de codigos vscode
-snap install okular             # leitor de PDF
+snap install code --classic	    	# editor de codigos vscode
+snap install okular             	# leitor de PDF
 snap install vlc 		        # reprodutor de video e musica
-snap install audacity 		    # edição de audio
+snap install audacity 		    	# edição de audio
 snap install gimp 		        # composição de fotos
-snap install darktable 	    	# editor de fotos
-snap install inkscape 		    # editor vetorial
-#snap install xmind 		    # mapa mental
-#snap install kdenlive 		    # editor de videos
-#snap install obs-studio    	# gravador de tela
+snap install darktable 	    		# editor de fotos
+snap install inkscape 		    	# editor vetorial
+#snap install xmind 		    	# mapa mental
+#snap install kdenlive 		    	# editor de videos
+#snap install obs-studio    		# gravador de tela
 
 
 echo "baixando e atualizando os pacotes"
