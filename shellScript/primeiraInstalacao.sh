0b7a216cb286e4108f04c0fd1bd73648518c4e94
ellian-aragao <ellian.aragao@gmail.com>
2020-02-21 10:13:07 -0300

    add comentários e implementando remoção de programas

--- shellScript/primeiraInstalacao.sh
+++ shellScript/primeiraInstalacao.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
 
-apt-get install virtualbox -y		# maquina virtual
-apt-get install gnome-tweak-tool -y	# configurar aparencia do gnome
-apt-get install vim -y		    	# editor de texto via terminal
-apt-get install git -y		    	# gerenciador de versionamento
-apt-get install qbittorrent -y  	# donwload de torrent
-apt-get install wireshark -y    	# analisador de pacotes da rede
-apt-get install calibre -y      	# editor e leitor de ebooks
-apt-get install default-jdk -y		# pacote java atual
+<<.deb
+    nesta parte encontram-se alguns programas que não haviam pacotes snaps 
+    e então foram baixados pelos repositórios padrões do ubuntu
+.deb
 
-snap install code --classic	    	# editor de codigos vscode
-snap install okular             	# leitor de PDF
-snap install vlc 		        # reprodutor de video e musica
-snap install audacity 		    	# edição de audio
-snap install gimp 		        # composição de fotos
-snap install darktable 	    		# editor de fotos
-snap install inkscape 		    	# editor vetorial
-#snap install xmind 		    	# mapa mental
-#snap install kdenlive 		    	# editor de videos
-#snap install obs-studio    		# gravador de tela
+echo "instalando pacotes .deb"
+apt-get install gcc -y                  # compilador C
+apt-get install virtualbox -y           # maquina virtual
+apt-get install gnome-tweak-tool -y     # configurar aparencia do gnome
+apt-get install vim -y                  # editor de texto via terminal
+apt-get install git -y                  # gerenciador de versionamento
+apt-get install qbittorrent -y          # donwload de torrent
+apt-get install wireshark -y            # analisador de pacotes da rede
+apt-get install calibre -y              # editor e leitor de ebooks
+apt-get install default-jdk -y          # pacote java atual
 
+<<snaps
+    usando a facilidade de gerenciar pacotes snaps visto a constante de atualizações
+    e independência do sistema tornando aplicável a qualquer distro
+snaps
+
+echo "instalando pacotes snap"
+snap install code --classic             # editor de codigos vscode
+snap install okular                     # leitor de PDF
+snap install vlc                        # reprodutor de video e musica
+snap install gimp                       # composição de fotos
+snap install darktable                  # editor de fotos
+snap install inkscape                   # editor vetorial
+#snap install audacity                  # edição de audio
+#snap install xmind                     # mapa mental
+#snap install kdenlive                  # editor de videos
+#snap install obs-studio                # gravador de tela
+
+<<padraoUbuntu
+    remove os pacotes básicos do ubuntu para utilização unica dos pacotes baixados 
+    dos quais me são mais agradáveis a usabilidade deles tornando futeis os outros
+padraoUbuntu
+
+echo "removendo pacotes .deb"
+apt-get remove evince -y                # remove leitor de PDF antigo
+apt-get remove aisleriot -y             # paciência
+apt-get remove gnome-mahjongg -y        # jogo
+apt-get remove gnome-mines -y           # jogo
+apt-get remove gnome-sudoku -y          # jogo
+apt-get remove gnome-todo -y            # lista todo
+apt-get remove totem -y                 # reprodutor de videos
+apt-get remove transmission-gtk -y      # torrent nativo ubuntu
+
+<<atualizando
+    atualizando o sistema por completo e removendo dependências do sistema
+atualizando
 
 echo "baixando e atualizando os pacotes"
 apt-get update && apt-get upgrade -y
