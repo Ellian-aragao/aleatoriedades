3d503a10608d840dea8b0daefc5f0bb4ecd9714b
ellian-aragao <ellian.aragao@gmail.com>
2020-02-26 14:36:59 -0300

    add estrutura de "emcapsulamento"

--- python/estudos/orientacaoAObjeto.py
+++ python/estudos/orientacaoAObjeto.py
@@ -6,12 +6,25 @@ class perfil(object):
         self.nome = nome
         self.numero = numero
         self.empresa = empresa
+        self.__curtidas = 0
     
     def imprime(self):
-        print ('nome: {}\nnumero: {}\nempresa: {}'.format(self.nome,self.numero,self.empresa))
-        
+        print ('nome: {}\nnumero: {}\nempresa: {}\ncurtidas: {}'.format(self.nome,self.numero,self.empresa,self.getCurtidas()))
+    
+    def curtir(self):
+        self.__curtidas += 1
+    
+    def getCurtidas(self):
+        return self.__curtidas
+    
 """
 # teste com o objeto
 user = perfil('robertinho','84028922','caelum')
+print ('curtidas: {}'.format(user.getCurtidas()))
+user.curtir()
+user.imprime()
+for i in range(10):
+    user.curtir()
+print('----------------------------')
 user.imprime()
 """
