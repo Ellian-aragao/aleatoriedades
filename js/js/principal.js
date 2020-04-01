b834696550f6c09d716a06decae5871b5be4eb79
ellian-aragao <ellian.aragao@gmail.com>
2020-03-31 21:12:45 -0300

    inicio dos estudos do JS

---
+++ js/js/principal.js
@@ -0,0 +1,5 @@
+console.log("arquivo importado com sucesso")
+var paciente = document.querySelector("paciente")
+paciente.querySelector("info-peso").textContent
+
+document.querySelector("paciente").querySelector("info-peso").textContent
