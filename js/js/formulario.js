29d554f56aef42a5569845e5a09c5bac96d0d1fc
ellian-aragao <ellian.aragao@gmail.com>
2020-04-01 22:17:40 -0300

    js do formulário

---
+++ js/js/formulario.js
@@ -0,0 +1,39 @@
+// verifica se o botão foi clicado
+document.querySelector("#adicionar-paciente").addEventListener("click", function(event) {
+    event.preventDefault()
+    var form = document.querySelector("#formularioDeAdicao")
+    // salva os valores do formulario
+    var nome    = form.nome.value
+    var peso    = form.peso.value
+    var altura  = form.altura.value
+    var gordura = form.gordura.value
+    // cria os elementos para serem injetados no HTML
+    var linha     = document.createElement("tr")
+    var nometd    = document.createElement("td")
+    var pesotd    = document.createElement("td")
+    var alturatd  = document.createElement("td")
+    var gorduratd = document.createElement("td")
+    var imctd     = document.createElement("td")
+    // add classe nos itens do paciente
+    linha.classList.add("paciente")
+    nometd.classList.add("info-nome")
+    pesotd.classList.add("info-peso")
+    alturatd.classList.add("info-altura")
+    gorduratd.classList.add("info-gordura")
+    imctd.classList.add("info-imc")    
+    // salva o conteúdo nas tags HTML
+    nometd.textContent    = nome
+    pesotd.textContent    = peso
+    alturatd.textContent  = altura
+    gorduratd.textContent = gordura    
+    // add as colunas na linha da tabela
+    linha.appendChild(nometd)
+    linha.appendChild(pesotd)
+    linha.appendChild(alturatd)
+    linha.appendChild(gorduratd)
+    linha.appendChild(imctd)
+    // add a linha linha da tabela no HTML
+    document.querySelector("#tabela-pacientes").appendChild(linha)
+    // add o imc do novo paciente
+    calcImc(linha)
+})
