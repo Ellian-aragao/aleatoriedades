9f0e3f7259cd780881f275c2c99e4751270ab810
ellian-aragao <ellian.aragao@gmail.com>
2020-04-03 02:37:07 -0300

    utilizando ajax e refatorando código

--- js/js/formulario.js
+++ js/js/formulario.js
@@ -1,27 +1,24 @@
 document.querySelector("#adicionar-paciente").addEventListener("click", function(event) {
     event.preventDefault()
     var dadosDoFormulario = document.querySelector("#formularioDeAdicao")
-    var linhaDoPaciente = criaTrDoPaciente(criaPacienteDoFormulario(dadosDoFormulario))
-    document.querySelector("#tabela-pacientes").appendChild(linhaDoPaciente)
+    addPacienteNaTabela(dadosDoFormulario, criaPacienteDoFormulario)
     dadosDoFormulario.reset()
 })
 
-function criaPacienteDoFormulario(form) {
-    var paciente = {
-        nome:    form.nome.value,
-        peso:    form.peso.value,
-        altura:  form.altura.value,
-        gordura: form.gordura.value,
-        imc:     calcImc(form.peso.value, form.altura.value)
-    }
-    return paciente
+function addPacienteNaTabela(paciente, funcaoEstruturaPaciente) {
+    var linhaDoPaciente = criaTrDoPaciente(funcaoEstruturaPaciente(paciente))
+    document.querySelector("#tabela-pacientes").appendChild(linhaDoPaciente)
 }
 
-function criaTd(classhtml, value) {
-    var coluna = document.createElement("td")
-    coluna.classList.add(classhtml)
-    coluna.textContent = value
-    return coluna
+function criaPacienteDoFormulario(paciente) {
+    var pacienteObject = {
+        nome:    paciente.nome.value,
+        peso:    paciente.peso.value,
+        altura:  paciente.altura.value,
+        gordura: paciente.gordura.value,
+        imc:     calcImc(paciente.peso.value, paciente.altura.value)
+    }
+    return pacienteObject
 }
 
 function criaTrDoPaciente(paciente) {
@@ -33,4 +30,11 @@ function criaTrDoPaciente(paciente) {
     linha.appendChild(criaTd("info-gordura", paciente.gordura))
     linha.appendChild(criaTd("info-imc", paciente.imc))
     return linha
+}
+
+function criaTd(classhtml, value) {
+    var coluna = document.createElement("td")
+    coluna.classList.add(classhtml)
+    coluna.textContent = value
+    return coluna
 }
