2be9412488716357b5c5119c11bebf6d4fba0987
ellian-aragao <ellian.aragao@gmail.com>
2020-04-05 23:59:11 -0300

    organizando os itens

--- js/js/removeItemDaLista.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#tabela-pacientes").addEventListener("dblclick", table => {
-    table.target.parentNode.classList.add("fadeOut")
-    setTimeout(() => {
-        table.target.parentNode.remove()
-    }, 500)
-})
