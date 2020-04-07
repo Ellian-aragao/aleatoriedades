55c2180ad29f1ea949d14bea5965cb5cddda553b
ellian-aragao <ellian.aragao@gmail.com>
2020-04-06 22:35:49 -0300

    termino do primeiro módulo js avançado

---
+++ js/avançado/1 - avançado - aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,46 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document)
+        let formulario = $(".form")
+
+        this._formData = formulario.data
+        this._formQuantidade = formulario.quantidade
+        this._formValor = formulario.valor
+
+
+        this._listaNegociacoes = new ListaNegociacao()
+        this._mensagem = new Mensagem()
+        
+        this._negociacaoView = new NegociacoesView($("#negociacoesView"))
+        this._mensagemView = new MensagemView($('#mensagemView'))
+        
+        this._negociacaoView.update(this._listaNegociacoes)
+        this._mensagemView.update(this._mensagem)
+    }
+    
+    adiciona(event) {
+        event.preventDefault()
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._negociacaoView.update(this._listaNegociacoes)
+        this._mensagem.texto = 'Negociação adicionada com sucesso'
+        this._mensagemView.update(this._mensagem)
+        this._limpaFormulario()
+    }
+
+    _criaNegociacao() {
+        return (new Negociacao(
+            new Date(DateHelper.stringToDate(this._formData.value)),
+            this._formQuantidade.value,
+            this._formValor.value
+        ))
+    }
+
+    _limpaFormulario() {
+        this._data = ''
+        this._quantidade = 1
+        this._valor = 0.0
+
+        // this._data.focus()
+    }
+}
