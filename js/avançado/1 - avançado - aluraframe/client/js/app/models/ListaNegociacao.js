0171fd8843d7a2b07d320305e23f09def24d735b
ellian-aragao <ellian.aragao@gmail.com>
2020-04-07 23:13:44 -0300

    removendo código de infraestrutura

--- js/avançado/1 - avançado - aluraframe/client/js/app/models/ListaNegociacao.js
+++ js/avançado/1 - avançado - aluraframe/client/js/app/models/ListaNegociacao.js
@@ -1,7 +1,6 @@
 class ListaNegociacao {
-    constructor(negociacaoView) {
+    constructor() {
         this._negociacoes = []
-        this._negociacaoViewParameter = negociacaoView
     }
     
     get negociacoes() {
@@ -10,11 +9,9 @@ class ListaNegociacao {
     
     adiciona(negociacao) {
         this._negociacoes.push(negociacao)
-        this._negociacaoViewParameter(this)
     }
 
     esvazia() {
         this._negociacoes = []
-        this._negociacaoViewParameter(this)
     }
 }
