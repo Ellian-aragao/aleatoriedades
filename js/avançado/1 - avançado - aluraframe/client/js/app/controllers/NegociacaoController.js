f8d4caa23ef35035f40bd54b458dcdc1acb65c63
ellian-aragao <ellian.aragao@gmail.com>
2020-04-07 23:13:54 -0300

    criando solução proxy

--- js/avançado/1 - avançado - aluraframe/client/js/app/controllers/NegociacaoController.js
+++ js/avançado/1 - avançado - aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -10,14 +10,28 @@ class NegociacaoController {
 
 
         this._mensagem = new Mensagem()
-        this._listaNegociacoes = new ListaNegociacao( 
-            model => this._negociacaoView.update(model)
-        )
+        // this._listaNegociacoes = new ListaNegociacao()
+
+        let self = this
+        this._listaNegociacoes = new Proxy(new ListaNegociacao(), {
+            get(target, prop, receiver) {
+                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function(){
+                        console.log(`método '${prop}' interceptado`);
+
+                        Reflect.apply(target[prop], target, arguments);
+
+                        self._negociacaoView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        });
         
         this._negociacaoView = new NegociacoesView($("#negociacoesView"))
         this._mensagemView = new MensagemView($('#mensagemView'))
         
-        // this._negociacaoView.update(this._listaNegociacoes)
+        this._negociacaoView.update(this._listaNegociacoes)
         this._mensagemView.update(this._mensagem)
     }
 
