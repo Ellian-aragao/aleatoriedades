39c37ce87a6b8b39f85f9688e6c08c45b5fe72c6
ellian-aragao <ellian.aragao@gmail.com>
2020-04-23 03:38:13 -0300

    program for implements the cp shell function

---
+++ c/copy.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define READ "r"
+#define WRITE "w"
+
+int copy(char* inputPath, char* outputPath) {
+    FILE *inputFile  = fopen(inputPath,"r");
+    FILE *outputFile = fopen(outputPath, "w");
+
+
+    fclose(inputFile);
+    fclose(outputFile);
+    return -1;
+}
+
+FILE* fileDeclaration(const char *path, const char *mode) {
+    FILE *ptr = fopen(path, mode);
+    if (ptr == NULL) {
+        puts("error");
+        exit(1);
+    }
+    return ptr;
+}
+
+int main(int argc, char* argv[]) {
+    
+    char name[] = "C Language";
+    unsigned long tam_name = sizeof(name) / sizeof(name[0]);
+    char name_cpy[tam_name];
+
+    FILE *ptr = fileDeclaration(argv[1], READ);
+
+    while (ptr)
+    {
+        /* code */
+    }
+    
+
+    fclose(ptr);
+
+    memcpy(name_cpy, name, tam_name);
+    printf("%s\n",name_cpy);
+
+    return 0;
+}
