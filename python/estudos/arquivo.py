5054fb28a99da78c846c7d3326339ac3aaf68ace
ellian-aragao <ellian.aragao@gmail.com>
2020-04-25 01:54:09 -0300

    removendo arquivos de estudos

--- python/estudos/arquivo.py
+++
@@ -1,6 +0,0 @@
-arquivo = open('db.csv','r')
-linhas = arquivo.readlines()
-for linha in linhas:
-    palavras = linha.split(',')
-    
-arquivo.close()
--- python/estudos/regex.py
+++
@@ -1,5 +0,0 @@
-import re
-
-palavra = 'Python, Cython or Jython'
-texto = re.findall('[A-Za-z]y[A-Za-z]+',palavra)
-print(texto)
