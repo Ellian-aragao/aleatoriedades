7a0332ac104eb97d70fa2074df1be68b0663da6e
ellian-aragao <ellian.aragao@gmail.com>
2020-04-25 01:52:25 -0300

    testando tratamento de erros de compilação

--- c/compile.sh
+++ c/compile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 gcc copy.c -Wall -o q
 if [ $? -eq 0 ]; then
-    echo ""
-    ./q copy.c
+
+    errorMensage=$(./q copy.c 2>&1)
+    if [ $? -ne 0 ]; then
+        echo $errorMensage
+    fi    
     rm q
 fi
