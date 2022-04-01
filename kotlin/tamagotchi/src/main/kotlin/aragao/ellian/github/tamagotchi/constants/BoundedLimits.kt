251df67673860cb8d4450d73a1dd9e49ec99a4ff
Ellian Aragão Dias <ellian.aragao@gmail.com>
2022-04-01 12:31:19 -0300

    iniciando projeto tamagotchi com kotlin

---
+++ kotlin/tamagotchi/src/main/kotlin/aragao/ellian/github/tamagotchi/constants/BoundedLimits.kt
@@ -0,0 +1,10 @@
+package aragao.ellian.github.tamagotchi.constants
+
+object BoundedLimits {
+    object Tamagotchi {
+        const val UP_LIMIT = 15;
+        const val DOWN_LIMIT = 0;
+        const val MAX_AGE = 100;
+        const val HUNGER_LIMIT = UP_LIMIT
+    }
+}
