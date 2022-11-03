2ac17d7ec2856528aab7bcfc88d7af601822b401
Ellian Aragão Dias <ellian@pop-os.localdomain>
2022-11-03 13:13:21 -0300

    feat: ignoring non letters and offset aplication

--- rust/cipher_cesar/src/main.rs
+++ rust/cipher_cesar/src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-fn encrypt(key_offset: i8, phrase: &String) -> String {
+fn encrypt(key_offset: &i8, phrase: &String) -> String {
     let mut encrypted_phrase = String::new();
     for c in phrase.chars() {
-        if jump_space(c, &mut encrypted_phrase) {
+        if ignore_non_letters(c, &mut encrypted_phrase) {
             continue;
         }
         let encrypted_char = shift_char(c, key_offset);
@@ -12,47 +12,63 @@ fn encrypt(key_offset: i8, phrase: &String) -> String {
     encrypted_phrase
 }
 
-fn decrypt(key_offset: i8, phrase: &String) -> String {
+fn decrypt(key_offset: &i8, phrase: &String) -> String {
     let mut decrypted_phrase = String::new();
     for c in phrase.chars() {
-        if jump_space(c, &mut decrypted_phrase) {
+        if ignore_non_letters(c, &mut decrypted_phrase) {
             continue;
         }
-        let decrypted_char = shift_char(c, -key_offset) as char;
+        let negative_key_offset = -key_offset;
+        let decrypted_char = shift_char(c, &negative_key_offset) as char;
         decrypted_phrase.push(decrypted_char);
     }
     decrypted_phrase
 }
 
-fn jump_space(c: char, phrase: &mut String) -> bool {
-    if c != ' ' {
-        return false;
+fn ignore_non_letters(c: char, phrase: &mut String) -> bool {
+    match c {
+        'a'..='z' | 'A'..='Z' => false,
+        __ => {
+            phrase.push(c);
+            true
+        }
     }
-    phrase.push(char::from(c));
-    return true;
 }
 
-fn shift_char(c: char, key_offset: i8) -> char {
-    let mut shifted_char = c.to_ascii_lowercase() as i8 + key_offset;
+fn shift_char(c: char, key_offset: &i8) -> char {
+    let unsigned_char_value = c.to_ascii_lowercase() as u8;
+    let mut shifted_char: u8;
+    if *key_offset < 0 {
+        shifted_char = unsigned_char_value - key_offset.abs() as u8;
+    } else {
+        shifted_char = unsigned_char_value + *key_offset as u8;
+    }
 
-    if shifted_char < ('a' as i8) {
+    if shifted_char < ('a' as u8) {
         shifted_char = shifted_char + 26;
     }
 
-    if shifted_char > ('z' as i8) {
+    if shifted_char > ('z' as u8) {
         shifted_char = shifted_char - 26;
     }
 
-    (shifted_char as u8) as char
+    shifted_char as char
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let key_offset = args[1].parse::<i8>().unwrap();
+    let key_offset = match args[1].parse::<i8>() {
+        Ok(n) => match n {
+            1..=25 => n,
+            __ => panic!("Please enter a valid number (1..25)"),
+        },
+        Err(_) => panic!("Please enter a valid number (1..25)"),
+    };
+    println!("      key offset: {}", key_offset);
     let phrase = args[2].to_string();
-    let encrypted_phrase = encrypt(key_offset, &phrase);
-    println!("entry phrase: {}", &phrase);
+    println!("    entry phrase: {}", phrase);
+    let encrypted_phrase = encrypt(&key_offset, &phrase);
     println!("Encrypted phrase: {}", encrypted_phrase);
-    let decrypted_phrase = decrypt(key_offset, &phrase);
+    let decrypted_phrase = decrypt(&key_offset, &phrase);
     println!("Decrypted phrase: {}", decrypted_phrase);
 }
