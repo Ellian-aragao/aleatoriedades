124204397ee95d1ca1024334e0125d6808eb610b
Ellian Aragão Dias <ellian.aragao@gmail.com>
2022-11-03 19:56:20 -0300

    feat: suporting upper and lower case

--- rust/cipher_cesar/src/main.rs
+++ rust/cipher_cesar/src/main.rs
@@ -1,25 +1,24 @@
 use std::env;
 
-fn encrypt(key_offset: &i8, phrase: &String) -> String {
+fn encrypt(key_offset: &u8, phrase: &String) -> String {
     let mut encrypted_phrase = String::new();
     for c in phrase.chars() {
         if ignore_non_letters(c, &mut encrypted_phrase) {
             continue;
         }
-        let encrypted_char = shift_char(c, key_offset);
+        let encrypted_char = shift_char(c, &(*key_offset as i8));
         encrypted_phrase.push(encrypted_char);
     }
     encrypted_phrase
 }
 
-fn decrypt(key_offset: &i8, phrase: &String) -> String {
+fn decrypt(key_offset: &u8, phrase: &String) -> String {
     let mut decrypted_phrase = String::new();
     for c in phrase.chars() {
         if ignore_non_letters(c, &mut decrypted_phrase) {
             continue;
         }
-        let negative_key_offset = -key_offset;
-        let decrypted_char = shift_char(c, &negative_key_offset) as char;
+        let decrypted_char = shift_char(c, &-(*key_offset as i8)) as char;
         decrypted_phrase.push(decrypted_char);
     }
     decrypted_phrase
@@ -35,29 +34,45 @@ fn ignore_non_letters(c: char, phrase: &mut String) -> bool {
     }
 }
 
-fn shift_char(c: char, key_offset: &i8) -> char {
-    let unsigned_char_value = c.to_ascii_lowercase() as u8;
-    let mut shifted_char: u8;
-    if *key_offset < 0 {
-        shifted_char = unsigned_char_value - key_offset.abs() as u8;
-    } else {
-        shifted_char = unsigned_char_value + *key_offset as u8;
-    }
+fn shift_char(char_to_shift: char, key_offset: &i8) -> char {
+    let shifted_char = shift_char_by_offset_absolute(&char_to_shift, &key_offset);
+    let shifted_char_bounded = shift_char_bounded(&char_to_shift, &shifted_char);
+    shifted_char_bounded as char
+}
 
-    if shifted_char < ('a' as u8) {
-        shifted_char = shifted_char + 26;
+fn shift_char_by_offset_absolute(char_to_shift: &char, key_offset: &i8) -> u8 {
+    if key_offset.is_positive() {
+        return *char_to_shift as u8 + key_offset.abs() as u8;
     }
+    return *char_to_shift as u8 - key_offset.abs() as u8;
+}
 
-    if shifted_char > ('z' as u8) {
-        shifted_char = shifted_char - 26;
+fn shift_char_bounded(c: &char, shifted_char: &u8) -> u8 {
+    if c.is_ascii_lowercase() {
+        return shift_char_bounded_checker(&shifted_char, 'a', 'z');
     }
+    return shift_char_bounded_checker(&shifted_char, 'A', 'Z');
+}
 
-    shifted_char as char
+fn shift_char_bounded_checker(
+    shifted_char: &u8,
+    left_bound_letter: char,
+    right_bound_letter: char,
+) -> u8 {
+    let delta_letters = 26;
+
+    if *shifted_char > right_bound_letter as u8 {
+        return *shifted_char - delta_letters;
+    }
+    if *shifted_char < left_bound_letter as u8 {
+        return *shifted_char + delta_letters;
+    }
+    return *shifted_char;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let key_offset = match args[1].parse::<i8>() {
+    let key_offset = match args[1].parse::<u8>() {
         Ok(n) => match n {
             1..=25 => n,
             __ => panic!("Please enter a valid number (1..25)"),
