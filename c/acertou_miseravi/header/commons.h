14ea16bb9a38b4221cd47865adfc9ae049a4218b
Ellian Aragão Dias <ellian.aragao@gmail.com>
2023-09-14 11:56:02 -0300

    feat: declaring function to global use

--- c/acertou_miseravi/header/commons.h
+++ c/acertou_miseravi/header/commons.h
@@ -9,6 +9,8 @@ typedef struct SizeStrings
   int operator2;
 } SizeStrings;
 
+int extract_value_from_operator_iteration(const char *operador, int iteration_operator);
+
 SizeStrings extract_values_from_operators_iteration(const char *operador1, const char *operador2, int iteration_operator1, int iteration_operator2);
 
 #endif
