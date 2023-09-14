98fbbfb26aa017353c4755e5ef88550f6ac4cbc7
Ellian Aragão Dias <ellian.aragao@gmail.com>
2023-09-14 11:54:24 -0300

    feat: extract function from process operator single

--- c/acertou_miseravi/src/commons.c
+++ c/acertou_miseravi/src/commons.c
@@ -1,16 +1,19 @@
 #include "../header/commons.h"
 
+int extract_value_from_operator_iteration(const char *operador, int iteration_operator)
+{
+  char operador_slice[2];
+
+  operador_slice[0] = operador[iteration_operator - 1];
+  operador_slice[1] = '\0';
+
+  return atoi(operador_slice);
+}
+
 SizeStrings extract_values_from_operators_iteration(const char *operador1, const char *operador2, int iteration_operator1, int iteration_operator2)
 {
-  char operador1_slice[2], operador2_slice[2];
-
-  operador1_slice[0] = operador1[iteration_operator1 - 1];
-  operador2_slice[0] = operador2[iteration_operator2 - 1];
-  operador1_slice[1] = '\0';
-  operador2_slice[1] = '\0';
-
-  int valor1 = atoi(operador1_slice);
-  int valor2 = atoi(operador2_slice);
+  int valor1 = extract_value_from_operator_iteration(operador1, iteration_operator1);
+  int valor2 = extract_value_from_operator_iteration(operador2, iteration_operator2);
   return (SizeStrings){valor1,
                        valor2};
 }
